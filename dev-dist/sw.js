7eb360366040fe9acef9f99df62f35b548ac7dd4
abrahamgregorius <athioii6@gmail.com>
2026-07-10 21:52:06 +0700

    feature: inventaris

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-afac4cd2'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.9ch3r35a2do"
+    "revision": "0.f9fgus7hmpg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
